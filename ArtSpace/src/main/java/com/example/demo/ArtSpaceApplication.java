d0b3c1779943ad2ccfcb3114d04bd4876f5b592d
Hannamug <namug1105@gmail.com>
2024-03-14 19:14:16 +0900

    2024_03_14_New

---
+++ ArtSpace/src/main/java/com/example/demo/ArtSpaceApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtSpaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtSpaceApplication.class, args);
+	}
+
+}
