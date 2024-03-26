9fb70799b6edc85767833710b6f3a428f9707f49
jhlee0227 <joon8019@gmail.com>
2024-03-26 17:35:32 +0900

    이준호 마이페이지 패스워드 유효성검사 스크립트

--- ArtSpace/src/main/java/com/example/demo/mypage/controller/MypageController.java
+++ ArtSpace/src/main/java/com/example/demo/mypage/controller/MypageController.java
@@ -1,5 +1,7 @@
 package com.example.demo.mypage.controller;
 
+import java.time.LocalDate;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,8 +12,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.demo.hall.dto.HallDTO;
-import com.example.demo.mypage.dto.MypageDTO;
 import com.example.demo.mypage.service.MypageService;
+import com.example.demo.user.dto.UserDTO;
 
 @Controller
 @RequestMapping("mypage")
@@ -20,49 +22,64 @@ public class MypageController {
 	@Autowired
 	MypageService mypageService;
 	
-	
+	// 마이페이지 기본
 	@GetMapping("")
 	public String mypage(Model model) {
-		model.addAttribute("my_info", new MypageDTO());
+		model.addAttribute("my_info", new UserDTO());
 		return "html/mypage";
 	}
 	
+	// 로그인 회원 마이페이지
 	@GetMapping("/{id}")
 	public String mpage(Model model, @PathVariable("id") Integer id) {
-		MypageDTO myInfo = mypageService.findByID(id);
+		UserDTO myInfo = mypageService.findByID(id);
 		model.addAttribute("my_info", myInfo);
+		model.addAttribute("id", id);
 		return "html/mypage";
 	}
 	
+	// 공연자 정보
 	@GetMapping("/performer")
 	public String performer() {
 		return "html/performer_info";
 	}
 	
+	// 내 즐겨찾기
 	@GetMapping("/favorite")
 	public String favorite() {
 		return "html/my_favorites";
 	}
 	
+	// 예약 내역
 	@GetMapping("/reserve")
 	public String reserve() {
 		return "html/reservation_list";
 	}
 	
+	// 이용 내역
 	@GetMapping("/uselist")
 	public String uselist() {
 		return "html/use_list";
 	}
 	
+	// 리뷰 작성 및 작성 내역
 	@GetMapping("/review")
 	public String review() {
 		return "html/my_review";
 	}
 	
-	@PostMapping("/{id}")
-	public String updateNickname(@ModelAttribute MypageDTO dto, @PathVariable Integer id) {
-		mypageService.updateNickname(dto);
-		
-		return "redirect:html/mypage";
+	// 닉네임 수정
+	@PostMapping("update/nickname/{id}")
+	public String updateNickname(@ModelAttribute UserDTO dto, @PathVariable("id") Integer id) {	
+		mypageService.updateNickname(dto, id);
+		return "redirect:/mypage/" + id;
 	}
+	
+	// 패스워드 수정
+	@PostMapping("update/pw/{id}")
+	public String updatePw(@ModelAttribute UserDTO dto, @PathVariable("id") Integer id) {
+		mypageService.updatePw(dto, id);
+		return "redirect:/mypage/" + id;
+	}
+	
 }
