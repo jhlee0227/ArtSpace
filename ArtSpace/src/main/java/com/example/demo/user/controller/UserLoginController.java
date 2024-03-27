357fcb5d36c9da567e1d83d402bd42f6471d2c2e
kimminseo123 <minseo1058@naver.com>
2024-03-27 15:03:24 +0900

    김민서-로그인백단

--- ArtSpace/src/main/java/com/example/demo/user/controller/UserLoginController.java
+++ ArtSpace/src/main/java/com/example/demo/user/controller/UserLoginController.java
@@ -1,8 +1,17 @@
 //package com.example.demo.user.controller;
 //
+//import org.springframework.stereotype.Controller;
 //import org.springframework.web.bind.annotation.PostMapping;
 //import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.servlet.ModelAndView;
 //
+//import com.example.demo.user.dto.MemberDTO;
+//import com.example.demo.user.service.memberService;
+//
+//import jakarta.servlet.http.HttpSession;
+//
+//@Controller
 //public class UserLoginController {
 //
 //	
