a3c299328a39bee2e72e58e90ef33e7de25199c8
jhlee0227 <joon8019@gmail.com>
2024-03-27 10:52:00 +0900

    이준호 공연자 정보 등록/수정 진행중..

--- ArtSpace/src/main/java/com/example/demo/mypage/dao/MypageDAO.java
+++ ArtSpace/src/main/java/com/example/demo/mypage/dao/MypageDAO.java
@@ -4,6 +4,7 @@ import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import com.example.demo.mypage.dto.PerformerDTO;
 import com.example.demo.user.dto.UserDTO;
 
 @Repository
@@ -26,5 +27,19 @@ public class MypageDAO {
 		sqlSession.update("mypage.updatePw", dto);
 		
 	}
+
+	public PerformerDTO findByPID(Integer id) {
+		// TODO Auto-generated method stub
+		return sqlSession.selectOne("mypage.findByPID", id);
+	}
+
+
+	public void insertPerformer(PerformerDTO dto) {
+		sqlSession.insert("mypage.insertPerformer", dto);
+	}
+
+//	public void updatePerformer(PerformerDTO dto) {
+//		sqlSession.update("mypage.updatePerformer", dto);
+//	}
 
 }
