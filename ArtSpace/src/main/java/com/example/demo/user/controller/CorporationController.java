24ab0391dccf6e2832a16b6afd47f80600917e2c
seoeev <mlmcild0905@gmail.com>
2024-04-01 18:01:35 +0900

    0401 법인회원가입 추가

--- ArtSpace/src/main/java/com/example/demo/user/controller/CorporationController.java
+++ ArtSpace/src/main/java/com/example/demo/user/controller/CorporationController.java
@@ -2,10 +2,15 @@ package com.example.demo.user.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.example.demo.user.dto.CorporationDTO;
+import com.example.demo.user.dto.UserDTO;
 import com.example.demo.user.service.CorporationService;
 
 @Controller
@@ -17,24 +22,48 @@ public class CorporationController {
 	
 	// 법인 회원가입인증 화면 띄움
 	@GetMapping("corporation")
-	public String checkCor() {
+	public String showCorporationCode() {
 		return "html/login/login_corporation";
 	} 
-	
-	// 법인 데이터 회원가입 화면 띄움
-	@PostMapping("join")
-	public String joinCor() {
+
+	// 법인 데이터 체크하고 정보 입력창으로
+	@PostMapping("corporation_code/insert")
+	public String checkCorCode(Model model, @RequestParam("company_number") String companyNumber) {
+		UserDTO user = new UserDTO();
+		user.setAuthority("SC");
+		corporationService.insertUser(user);
+		
+		CorporationDTO corpor = new CorporationDTO();
+		corpor.setCompany_number(companyNumber);
+		corpor.setUser_id(user.getUser_id());
+		
+		corporationService.insertCompanyNumber(corpor);
+		
+		model.addAttribute("user_id", user.getUser_id());
+		
 		return "html/login/login_joincorporation";
-	} 
+	}
+		
+	@PostMapping("insert")
+	public String scInsert(@ModelAttribute UserDTO userDTO) {
+		corporationService.updateUser(userDTO);
+		return "redirect:/cor/join_code";
+	}
+	
 	
 	// 법인 회원가입인증코드 화면 띄움
-	@PostMapping("joinCode")
+	@GetMapping("join_code")
 	public String codeCor() {
 		return "html/login/login_code_corporation";
 	} 
 	
+	@PostMapping("joincode_check")
+	public String codeCheck() {
+		return "redirect:/cor/welcome";
+	}
+	
 	// 법인 회원가입 완료 화면 띄움
-	@PostMapping("welcome")
+	@GetMapping("welcome")
 	public String welcomeCor() {
 		return "html/login/login_welcome_corporation";
 	} 
