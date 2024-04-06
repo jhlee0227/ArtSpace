83d1768f88cd2b7b20ca6225d0d701c34c92b622
kimminseo123 <minseo1058@naver.com>
2024-04-06 19:16:48 +0900

    김민서-문의사항백단

--- ArtSpace/src/main/java/com/example/demo/announcement/service/AskServiceImpl.java
+++ ArtSpace/src/main/java/com/example/demo/announcement/service/AskServiceImpl.java
@@ -2,12 +2,17 @@ package com.example.demo.announcement.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.demo.announcement.dao.AskDao;
 import com.example.demo.announcement.dto.AskDto;
 @Service
 public class AskServiceImpl implements AskService{
 
+	@Autowired
+	AskDao askDao;
+	
 	@Override
 	public void insert1(AskDto askDTO) {
 		// TODO Auto-generated method stub
@@ -17,7 +22,7 @@ public class AskServiceImpl implements AskService{
 	@Override
 	public List<AskDto> getAsk() {
 		// TODO Auto-generated method stub
-		return null;
+		return askDao.getAsk();
 	}
 
 
