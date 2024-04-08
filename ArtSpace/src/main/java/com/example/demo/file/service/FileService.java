41492a8d07522a660b2211cec9f7b3596b3c00e4
seoeev <mlmcild0905@gmail.com>
2024-04-08 17:56:58 +0900

    0408 홀 이미지 업로드 구현중(업로드만)

--- ArtSpace/src/main/java/com/example/demo/file/service/FileService.java
+++ ArtSpace/src/main/java/com/example/demo/file/service/FileService.java
@@ -1,8 +1,13 @@
 package com.example.demo.file.service;
 
+import org.springframework.web.multipart.MultipartFile;
+
 import com.example.demo.file.dto.GCSRequest;
 
 public interface FileService {
 
 	void uploadObject(GCSRequest gcsRequest);
+
+	void uploadHallImage(MultipartFile[] files, Integer id);
+
 }
