35d134861ec12412a6204c95f93ee09175c8e4c9
seoeev <mlmcild0905@gmail.com>
2024-04-08 14:39:39 +0900

    ks 공연장 등록 폼 이미지 업로드 구현중(미완)

--- ArtSpace/src/main/java/com/example/demo/hall/dto/HallTimeDTO.java
+++ ArtSpace/src/main/java/com/example/demo/hall/dto/HallTimeDTO.java
@@ -1,10 +1,10 @@
 package com.example.demo.hall.dto;
 
 public class HallTimeDTO {
-	private Integer hall_id;
-	private String type;
-	private Integer price;
-	private boolean ischeck;
+	private Integer hall_id;	// 공연장ID
+	private String type;		// 무슨 시간대인지 (오전, 오후, 저녁, 하루)
+	private Integer price;		// 가격
+	private boolean ischeck;	// 체크했는지(작성 폼용)
 	
 	public Integer getHall_id() {
 		return hall_id;
